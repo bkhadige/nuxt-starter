e1891eef0c5b45fa8b1eb50d7c3b09bc6ca6093c
Bassem <bkhadige@gmail.com>
2023-02-13 07:11:21 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,13 +7,5 @@ export default defineNuxtConfig({
         '@nuxt/image-edge',
     ],
     image: {
-        provider: 'github',
-        github: {
-          baseURL: 'https://bkhadige.github.io/nuxt-starter',
-          modifiers: {
-            effect: 'sharpen:100',
-            quality: 'auto:best',
-          }
-        }
-      }
+    }
 })
